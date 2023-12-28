b1b6cc4c44cec338ceab79974ec86f3303e14a22
sodiqovsodiqjon <115532144+sodiqovsodiqjon@users.noreply.github.com>
2023-12-28 17:23:21 +0500

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import Layouts from "./layouts";
 import reportWebVitals from "./reportWebVitals";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
